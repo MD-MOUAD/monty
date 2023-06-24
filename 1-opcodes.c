b3ab6686b4eba6d408f56fb686d1aacd11619818
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-06-25 00:19:09 +0100

    monty first task

--- 1-opcodes.c
+++ 1-opcodes.c
@@ -28,7 +28,7 @@ void add(stack_t **stack, unsigned int line_number)
  *
  * Return: (void)
  */
-void add(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number)
 {
 	/* Nothing */
 }
