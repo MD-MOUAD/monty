3ec807c568da5aba729e061b4c5278562415a0a7
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-06-25 01:59:23 +0100

    monty first task

--- 2-opcodes.c
+++ 2-opcodes.c
@@ -64,7 +64,7 @@ void pstr(stack_t **stack, unsigned int line_number)
 	{
 		if (tmp->n <= 0 || tmp->n > 127)
 			break;
-		printf("%c\n", tmp->n);
+		printf("%c", tmp->n);
 		tmp = tmp->next;
 	}
 	printf("\n");
